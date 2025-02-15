4330369db018b9f022761ea39995b8b5cb76cc0c
maksymilian <maksymiliangaleziowski@gmail.com>
2025-02-15 15:31:04 +0100

    Client Repository

--- OrderManagmentApi/OrderManagmentApi/Program.cs
+++ OrderManagmentApi/OrderManagmentApi/Program.cs
@@ -1,9 +1,19 @@
+using Application.Extentions;
+using Infrastructure.Context;
+using Infrastructure.Extentions;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+builder.Services.AddAplicationLayer();
 
 var app = builder.Build();
 
@@ -15,7 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 
 
 app.Run();
